f438462d95071daec2930a927bcc352493f6e61a
Shashiwadana Nirmani <shashiwadana_n@3logixs.lk>
2019-08-26 15:46:23 +0530

    component.html changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-const routes: Routes = [];
+import { EventsComponent } from './components/events/events.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { SpecialEventsComponent } from './components/special-events/special-events.component';
+const routes: Routes = [
+  
+  {
+    path:'',
+    redirectTo:'/events',
+    pathMatch:'full'
+  },
+
+  {
+    path:'components/events',
+    component:EventsComponent
+  },
+
+  {
+    path:'components/login',
+    component:LoginComponent
+  },
+  {
+    path:'components/registration',
+    component:RegistrationComponent
+  },
+  {
+    path:'components/special-events',
+    component:SpecialEventsComponent
+  },
+
+
+
+
+
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
