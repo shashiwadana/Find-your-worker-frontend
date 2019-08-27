98ff396f8dcd0ebee0ee761ea99fec8b27f58bd5
Shashiwadana Nirmani <shashiwadana_n@3logixs.lk>
2019-08-27 11:21:58 +0530

    basic signup all

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-
+  registerUserData={}
   constructor() { }
 
   ngOnInit() {
   }
-
+  registerUser(){
+    console.log(this.registerUserData) 
+  }
 }
