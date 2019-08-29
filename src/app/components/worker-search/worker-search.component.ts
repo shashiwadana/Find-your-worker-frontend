6bc287ce71d910b74658bea1434cba8e6e6271c5
Shashiwadana Nirmani <shashiwadana_n@3logixs.lk>
2019-08-29 14:34:56 +0530

    worker-search add

--- src/app/components/worker-search/worker-search.component.ts
+++ src/app/components/worker-search/worker-search.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
+import { Job } from '../../models/worker-search.model';
 @Component({
   selector: 'app-worker-search',
   templateUrl: './worker-search.component.html',
   styleUrls: ['./worker-search.component.scss']
 })
 export class WorkerSearchComponent implements OnInit {
+  jobs:Job[]=[
+    {id:1, name:'Plumber'},
+    {id:2, name:'Nanny'},
+    {id:3, name:'Doc'}
 
+  ];
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
