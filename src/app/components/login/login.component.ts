3ff17e9830e7b53ac0beae873182b04cad4c8bcc
Shashiwadana Nirmani <shashiwadana_n@3logixs.lk>
2019-09-03 11:43:01 +0530

    completed login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import{AuthService} from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+
+import { Router } from '@angular/router';
+import{FormBuilder,Validators} from '../../../../node_modules/@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginUserData={}
-  constructor(private _auth:AuthService) { }
-  //constructor() { }
+  form;
+  constructor(private _login:AuthService,
+              private fb:FormBuilder,
+              private toastr:ToastrService,
+              private router:Router,
+  ) {
+
+    this .form = this .fb .group({
+      username: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      password: ['', Validators.required]
+    });
+
+   }
+
+   loginSubmit() {
+    this._login.loginUser(this.username.value, this.password.value).subscribe(
+      result => {
+        if (result.status === 200 && result.message === 'Authorized') {
+          this.toastr.success('Login Success');
+          localStorage.setItem('sessionEmail', result.result.sessionEmail);
+          localStorage.setItem('sessionType', result.result.sessionType);
+          if (result.result.sessionType === 'worker') {
+            this.router.navigate(['worker']);
+          } else {
+            this.router.navigate(['']);
+          }
+        } else {
+          this.form.reset();
+          this.toastr.error('Login Failed');
+        }
+      }
+    );
+  }
+
+  
+
+
+
+
+
+
   ngOnInit() {
   }
+  get username() {return this.form.get('username'); }
+  get password() {return this.form.get('password'); }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,27 +2,45 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable } from '../../../node_modules/rxjs';
+import{LoginResponse,RegisterResponse} from '../models/user.model';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  //private _registerUrl="http://localhost:3000/user/register";
-  
-  
-  
-  
-  
   
   
   constructor(private http:HttpClient) { }
-  registerUser(username, password, mobile, type): Observable<any> {
-    return this.http.post<any>('http://localhost:3000/user/register', {
+  
+  loginUser(username, password): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>('http://localhost:3000/user/login', {UserEmail: username, Password: password});
+  }
+  registerUser(username, password, mobile, type): Observable<RegisterResponse> {
+    return this.http.post<RegisterResponse>('http://localhost:3000/user/register', {
       UserEmail: username,
       Password: password,
       ContactNumber: mobile,
       UserType : type
     });
   
-
 }
+logout() {
+  return this.http.post('http://localhost:3000/user/logout', {});
+}
+
+isLogged() {
+  if (localStorage.getItem('sessionEmail') === null) {
+    return false;
+  }
+  return true;
+}
+
+getUserType() {
+  return localStorage.getItem('sessionType');
+}
+
+
+
+
+
+
 }
